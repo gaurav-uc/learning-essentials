a84f6055cd2051f99891ec58046dc3f58e6ae40a
gaurav-uc <gauravp@unicourt.com>
2019-01-07 09:29:06 +0530

    Python script to extract the different case number prefixes

---
+++ extract_case_numbers.py
@@ -0,0 +1,16 @@
+import re
+CASE_NUMBER_PREFIX_REGEX_STRING = r'\d+(?P<prefix>[a-zA-Z]+)(?P<raw_case_number>\d+)'
+
+case_prefix_list = []
+
+la_case_prefix_regex = re.compile(CASE_NUMBER_PREFIX_REGEX_STRING, flags=re.I)
+case_numbers_file = open("case_numbers.txt", "r")
+
+for case_numbers in case_numbers_file:
+	la_case_prefix = re.search(la_case_prefix_regex, case_number).group('prefix')
+	case_prefix_list.append(la_case_prefix)
+
+for unique_prefixes in set(case_prefix_list):
+	print unique_prefixes,
+
+
