b12ad97541cf2d56389d6a182603d0076907abfe
gaurav-uc <gauravp@unicourt.com>
2019-01-15 12:49:46 +0530

    Download bar with hyphen inbetween

--- extract_case_numbers.py
+++ extract_case_numbers.py
@@ -1,9 +1,15 @@
 import re
-CASE_NUMBER_PREFIX_REGEX_STRING = r'\d+(?P<prefix>[a-zA-Z]+)(?P<raw_case_number>\d+)'
+CA_LA1_CASE_NUMBER_PREFIX_REGEX_STRING = r'\d+(?P<prefix>[a-zA-Z]+)(?P<raw_case_number>\d+)'
+FL_PICC_CASE_NUMBER_PREFIX_REGEX_STRING = r'\d*-\d*-(?P<suffix>[A-Z]{2})'
+
+# SELECT case_number FROM courtcase c
+# INNER JOIN party p ON p.case_id=c.id
+# WHERE p.fname='NONE';
+# sudo mysql -u codaxtr_user codaxtr -pc0d@xtr -e "SELECT case_number FROM courtcase c INNER JOIN party p ON p.case_id=c.id WHERE p.fname=\'NONE\';" > case_numbers.txt
 
 case_prefix_list = []
 
-la_case_prefix_regex = re.compile(CASE_NUMBER_PREFIX_REGEX_STRING, flags=re.I)
+la_case_prefix_regex = re.compile(FL_PICC_CASE_NUMBER_PREFIX_REGEX_STRING, flags=re.I)
 case_numbers_file = open("case_numbers.txt", "r")
 
 for case_numbers in case_numbers_file:
