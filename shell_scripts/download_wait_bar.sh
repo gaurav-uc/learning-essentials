7b5ff7a33d1b6268e84a64ddff99f2c9505227ab
gaurav-uc <gauravp@unicourt.com>
2019-01-15 12:56:14 +0530

    Download bar with hyphen inbetween

---
+++ shell_scripts/download_wait_bar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in 0 1 2 3 4
+do
+	printf "\r \\"
+	sleep 1
+	printf "\r -"
+	sleep 1
+	printf "\r /"
+	sleep 1
+	printf "\r -"
+	sleep 1
+done
+printf "\nDownload Completed\n"
+
